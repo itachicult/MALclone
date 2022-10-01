a3c602e89d526cfec8e38386435b1ae03811619f
arjunvuppala123 <56397988+arjunvuppala123@users.noreply.github.com>
2022-10-01 17:29:13 +0530

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
-import {NavBar} from '../components/Navbar'
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
